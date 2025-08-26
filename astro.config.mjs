642e547c0e43149bd42a35ada8c24fc1b1e78a27
DigitalPoetics <diagrammaticreadings@hotmail.com>
2025-08-26 00:24:40 -0500

    searchUi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({
-        mode: 'standalone',
-  }),
-    output: 'server',
+  output: "server",
+  adapter: netlify(),
 });
+
