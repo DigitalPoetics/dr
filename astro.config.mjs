42ba36be0a73f33c1697b921ccfb8f008cf54357
DigitalPoetics <diagrammaticreadings@hotmail.com>
2025-09-01 06:51:10 -0500

    deleuzian

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+  integrations: [sentry(), spotlightjs()],
 });
-
