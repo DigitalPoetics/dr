87285759f4c101d498aa0db5994a02d9aa2f453c
DigitalPoetics <diagrammaticreadings@hotmail.com>
2025-09-03 06:00:53 -0500

    sentry removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [
-    ...(process.env.NODE_ENV === 'production' ? [sentry()] : []),
-    ...(process.env.NODE_ENV === 'development' ? [spotlightjs()] : [])
-  ],
+  integrations: [],
 });
