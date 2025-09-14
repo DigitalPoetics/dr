bc618513b29620d1f78fe2550a23eba81625040b
DigitalPoetics <diagrammaticreadings@hotmail.com>
2025-09-14 00:30:37 -0500

    blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import mdx from '@astrojs/mdx';
+
 
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: netlify(),
-  integrations: [],
+  integrations: [mdx()],
   build: {
     inlineStylesheets: "always",
   }
