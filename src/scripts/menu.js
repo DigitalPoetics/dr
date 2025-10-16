551bfa1353a889d00874a064f0ed45eccd466b78
DigitalPoetics <diagrammaticreadings@hotmail.com>
2025-10-15 22:11:10 -0500

    footer image for pagefind results image

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,12 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // Toggle nav-bar on hamburger click
   hamburger.addEventListener('click', () => {
     navBar.classList.toggle('expanded');
+    hamburger.classList.toggle('active');
   });
 
   // Close nav-bar when clicking outside
   document.addEventListener('click', (event) => {
     if (!navBar.contains(event.target) && !hamburger.contains(event.target) && navBar.classList.contains('expanded')) {
       navBar.classList.remove('expanded');
+      hamburger.classList.remove('active');
     }
   });
 });
