d5aa00cac7934be4f26d5be0269f67e530b66d5b
DigitalPoetics <diagrammaticreadings@hotmail.com>
2026-08-21 22:37:42 +0545

    header and navigation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,19 +1,4 @@
-// hamburger menu toggle script
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.hamburger');
-  const navBar = document.querySelector('.nav-bar');
 
-  // Toggle nav-bar on hamburger click
-  hamburger.addEventListener('click', () => {
-    navBar.classList.toggle('expanded');
-    hamburger.classList.toggle('active');
-  });
 
-  // Close nav-bar when clicking outside
-  document.addEventListener('click', (event) => {
-    if (!navBar.contains(event.target) && !hamburger.contains(event.target) && navBar.classList.contains('expanded')) {
-      navBar.classList.remove('expanded');
-      hamburger.classList.remove('active');
-    }
-  });
-});
+
+
