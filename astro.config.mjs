09827bccc88ecca69adac3238f920322d9ffe0b6
DigitalPoetics <diagrammaticreadings@hotmail.com>
2026-08-31 01:58:39 +0545

    gephi-intro-setup post

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import pagefind from "astro-pagefind";
-
+import rehypeExternalLinks from "rehype-external-links";
 
 
 
@@ -15,6 +15,11 @@ export default defineConfig({
   adapter: netlify(),
   site: 'https://diagrammaticreadings.com',
   integrations: [expressiveCode(), mdx(), sitemap(), pagefind()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    ],
+  },
   build: {
     inlineStylesheets: "never",
   },
